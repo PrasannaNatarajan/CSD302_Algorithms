ab9898af1e7375e3fbbe0a9efbb8b0239a449744
= <prasanna2431997@gmail.com>
2017-01-29 15:31:20 +0530

    added time and changed into long ints

--- labAssignment1/ques3.c
+++ labAssignment1/ques3.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 int main(){
     int counter=0;
-    int n;
+    long int n;
     printf("Enter the number n: ");
-    scanf(" %d",&n);
-    int i;
+    scanf(" %ld",&n);
+    long int i;
     printf("divisors are: ");
+    clock_t begin = clock();
     for(i=1;i<=n/2;i++){
         if(n%i==0){
             counter+=i;
-            printf("%d ",i);fflush(stdout);
+            printf("%ld ",i);fflush(stdout);
         }
     }
     printf("\n");
@@ -23,6 +25,8 @@ int main(){
         printf("The number is not the sum of all its divisors\n");
         fflush(stdout);
     }
-
+    clock_t end = clock();
+    double time_spent = (double)(end-begin)/CLOCKS_PER_SEC;
+    printf("The time taken for n = %ld  is: %1f\n",n,time_spent);
     return 1;
 }
