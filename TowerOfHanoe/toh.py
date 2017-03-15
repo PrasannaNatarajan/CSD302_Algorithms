85b010ff77b8ed63da21bab980b34f6fddeb4b0f
Prasanna Natarajan <prasanna2431997@gmail.com>
2017-03-15 18:02:26 +0530

    Update toh.py

--- TowerOfHanoe/toh.py
+++ TowerOfHanoe/toh.py
@@ -1,4 +1,6 @@
+#a counter c to store number of iterations
 c = 0 
+#define a function for recursively solving toh problem
 def toh(n,f,t,o):
 	global c
 	if n == 1:
@@ -10,7 +12,8 @@ def toh(n,f,t,o):
 	c = c+1
 	toh(n-1,o,t,f)
 
-
+#the program driver
+#maybe make it a separate function
 n = int(raw_input("Enter the height of the tower at source: "))
 toh(n,'A','B','C')
 print "Number of steps = ",c
