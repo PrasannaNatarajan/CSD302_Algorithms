c79c10417031a8933bb66e4d6873280d571095cc
Prasanna Natarajan <prasanna2431997@gmail.com>
2017-03-15 18:03:33 +0530

    Update toh.py

--- TowerOfHanoe/toh.py
+++ TowerOfHanoe/toh.py
@@ -2,12 +2,16 @@
 c = 0 
 #define a function for recursively solving toh problem
 def toh(n,f,t,o):
+	#using the global variable c for counting the number of iterations
 	global c
+	#stopping condition for recursion
 	if n == 1:
 		print "move disk 1 from %c to %c" %(f,t)		 
 		c = c+1
 		return
+	#recursive call
 	toh(n-1,f,o,t)
+	#printing the 
 	print "move disk %d from %c to %c"%(n,f,t)
 	c = c+1
 	toh(n-1,o,t,f)
