76333b4b4c8ac575019c516fbc477ea2ac9e9f8c
= <prasanna2431997@gmail.com>
2017-03-22 18:27:07 +0530

    added comments to lab11

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # CSD302_Algorithms 
-This is a repository of lab assignments done for the course CSD:302 Design and Analysis of Algorithms
+This is a repository of lab assignments done for the course CSD:302 Design and Analysis of Algorithms.
+
+The different lab assignments have description within the .c or .py file.
 
 ### Contributor
 [Prasanna Natarajan](//github.com/PrasannaNatarajan)
--- lab11/lab11.c
+++ lab11/lab11.c
@@ -1,8 +1,9 @@
 /*
 Name    	:   Prasanna Natarajan
-Roll Number 	:   1410110298
-Inputs  	:   n = number of characters
-Outputs 	:   The huffman code for the given sequence
+Roll Number :   1410110298
+Inputs  	:   A vector p of length N
+Outputs 	:   Matrices with correct order of multiplication specified by brackets
+Description :   This algorithm is taken from Introduction to Algorithm By Cormen Pg.375 - 377
 */
 
 #include<stdio.h>
@@ -12,15 +13,22 @@ Outputs 	:   The huffman code for the given sequence
 #define N 8
 
 //Global variables
+
 int m[N+1][N+1];
 int s[N][N];
+
+//function declarations
+
 void matrixChainOrder(int p[]);
+void printOptimalParens(int i, int j);
 
 int main(){
 
-    int p[N] = {20, 10, 5, 35, 45, 5, 10, 15};
+    int p[N] = {20, 10, 5, 35, 45, 5, 10, 15}; // input p vector
+    // filling the global variables m and s
     matrixChainOrder(p);
     int i,j;
+    // printing m
     for(i=0;i<N;i++){
         for(j=0;j<N;j++){
             printf("%d ",m[i][j]);
@@ -28,13 +36,14 @@ int main(){
         printf("\n");
     }
     printf("\n");
+    // printing s
     for(i=0;i<N;i++){
         for(j=0;j<N;j++){
             printf("%d ",s[i][j]);
         }
         printf("\n");
     }
-
+    // printing the optimal order of multiplication for getting minimum cost
     printOptimalParens(1,N-1);
 }
 
@@ -52,14 +61,10 @@ void matrixChainOrder(int p[]){
             m[i][j] = INT_MAX;
             int k;
             for(k=i;k<j;k++){
-                //printf("i = %d ,j = %d\n",i,j);
                 int q = m[i][k] +m[k+1][j]+(p[i-1]*p[j]*p[k]);
-                //debug print
-                //printf("%d + %d + (%d*%d*%d)\n",m[i][k],m[k+1][j],p[i-1],p[j],p[k]);
                 if(q<m[i][j]){
                     m[i][j] = q;
                     s[i][j] = k;
-                    //printf("s[%d][%d] = %d\n",i,j,s[i][j]);
                 }
             }
         }
