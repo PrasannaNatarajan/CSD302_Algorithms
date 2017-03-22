b3055c53a9272a161feb2680a04e7ffd210f0a92
= <prasanna2431997@gmail.com>
2017-03-22 18:15:08 +0530

    added matrix chain multiplication :: lab11

---
+++ lab11/lab11.c
@@ -0,0 +1,81 @@
+/*
+Name    	:   Prasanna Natarajan
+Roll Number 	:   1410110298
+Inputs  	:   n = number of characters
+Outputs 	:   The huffman code for the given sequence
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+#define N 8
+
+//Global variables
+int m[N+1][N+1];
+int s[N][N];
+void matrixChainOrder(int p[]);
+
+int main(){
+
+    int p[N] = {20, 10, 5, 35, 45, 5, 10, 15};
+    matrixChainOrder(p);
+    int i,j;
+    for(i=0;i<N;i++){
+        for(j=0;j<N;j++){
+            printf("%d ",m[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for(i=0;i<N;i++){
+        for(j=0;j<N;j++){
+            printf("%d ",s[i][j]);
+        }
+        printf("\n");
+    }
+
+    printOptimalParens(1,N-1);
+}
+
+void matrixChainOrder(int p[]){
+    int i;
+    int n = N-1;
+    for(i=0;i<=n;i++){
+        m[i][i] = 0;
+    }
+
+    int l;
+    for(l=2;l<=n;l++){
+        for(i=1;i<=n-l+1;i++){
+            int j = i+l-1;
+            m[i][j] = INT_MAX;
+            int k;
+            for(k=i;k<j;k++){
+                //printf("i = %d ,j = %d\n",i,j);
+                int q = m[i][k] +m[k+1][j]+(p[i-1]*p[j]*p[k]);
+                //debug print
+                //printf("%d + %d + (%d*%d*%d)\n",m[i][k],m[k+1][j],p[i-1],p[j],p[k]);
+                if(q<m[i][j]){
+                    m[i][j] = q;
+                    s[i][j] = k;
+                    //printf("s[%d][%d] = %d\n",i,j,s[i][j]);
+                }
+            }
+        }
+    }
+}
+
+void printOptimalParens(int i, int j){
+    if(i==(j)){
+        printf("A%d",i);
+    }
+
+    else{
+        printf("(");
+        printOptimalParens(i,s[i][j]);
+        printOptimalParens(s[i][j]+1,j);
+        printf(")");
+    }
+
+}
