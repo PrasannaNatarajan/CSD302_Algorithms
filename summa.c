db396aa6cd437fd06fc525be419b9ff640b6d979
= <prasanna2431997@gmail.com>
2017-04-05 11:46:25 +0530

    renamed lab1 and lab2

---
+++ summa.c
@@ -0,0 +1,29 @@
+/*
+Name    	:   Prasanna Natarajan
+Roll Number :   1410110298
+Inputs  	:   The number of items,capacity of the knapsack and price and weights of each items
+Outputs 	:   The maximum value that can be taken
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+// #defines
+
+//global variables
+
+//function declarations
+
+int main(){
+
+
+    clock_t begin = clock();
+    // code that should be timed...
+    clock_t end = clock();
+    double time_spent = (double)(end-begin)/CLOCKS_PER_SEC;
+    printf("Time for execution of %dth input = %lf\n",i,time_spent);
+
+	return 0;
+}
+
